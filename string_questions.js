9ef1634393015000898b30661499a985ac3c16ac
Sukant Ghosh <sukantghosh@Sukants-MacBook-Air-2.local>
2022-12-13 19:45:50 +0000

    Adding more

--- string_questions.js
+++ string_questions.js
@@ -1,6 +1,10 @@
-// Questions : 
-// How can you reverse a string?
+//  Questions : 
 
+//  How can you reverse a string?
+//---------------------------------
+
+
+ /* 
 function revByFunction(str){
     return str.split("").reverse().join("");
 }
@@ -13,18 +17,57 @@ function revByLoop(str){
     return reversed;
 }
 
+function revByRecursion(str){
+    if (str === "")
+        return ""
+    else 
+        return revByRecursion(str.substring(1)) + str.charAt(0);
+}
 
-console.log(revByLoop('hello'));
+console.log(revByRecursion('hello'));
+*/
 
 
+// Check if a string is palindrome?
+//---------------------------------
 
 
-// What is a palindrome string?
+/*
+function pallindromeCheck(str){
+    let re = /[^A-Za-z0-9]/g;
+    let cleanedStr = str.toLowerCase().replace(re, '');
+    let reveresedStr = cleanedStr.split('').reverse().join('');
+    return reveresedStr===cleanedStr;   
+}
+console.log(pallindromeCheck("A man, a plan, a canal. Panama"));
+*/
+
 // How to get the matching characters in a string?
+
+
+
 // How to get the non-matching characters in a string?
+
+
+
 // How to calculate the number of vowels and consonants in a string?
+
+
+
 // How do you prove that the two strings are anagrams?
+
+
+
 // Find the count for the occurrence of a particular character in a string?
+
+
+
 // How to verify if two strings are a rotation mutually?
+
+
+
 // How to calculate the number of numerical digits in a string?
+
+
+
 // How to compute the first character of a string that is not repeated?
